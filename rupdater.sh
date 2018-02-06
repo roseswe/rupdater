8644fe1fc5a4466c8979eaaee9bf7b5142e9a210
roseswe <3810660+roseswe@users.noreply.github.com>
2018-02-06 21:16:15 +0100

    Added gitignore and rupdater.sh

---
+++ rupdater.sh
@@ -0,0 +1,30 @@
+# @(#) $Id: rupdater.sh,v 1.10 2017/12/11 07:50:46 ralph Exp $
+# --=-----------------------------------------------------------------------=---
+# (c) 1997 - 2018 by Ralph Roth  -*- http://rose.rult.at -*-
+
+# There is no integrated update mechanism for our programs. You can use the
+# shell script rupdater.sh if you have a *NIX environment to download updates
+# of the program.
+
+echo "ROSE SWE automatic updater for Linux or Win32/cygwin using wget/awk"
+
+URL=http://rose-swe.bplaced.net/dl
+#   -N,  --timestamping            don't re-retrieve files unless newer than
+wget -N $URL/md5sums.md5  || exit 1
+
+for i in rhbvs fmirc f_mirc md5test VirScanPlus MemScan CrCheck HackStop hashall
+do
+     FILE=$(gawk ' $2 ~ /'${i}'/  { print $2; }; ' md5sums.md5)
+     # echo "$i=$FILE" # debug
+     if [ ! -r "$FILE" ]
+     then
+        echo "Trying to download new/updated file $FILE (from the $i package)"
+        wget -N -c $URL/$FILE
+     else
+        echo "SORRY: $FILE: No newer version available!"
+     fi
+done
+
+# $Header: /home/cvs/src/prj/web_page/rupdater.sh,v 1.10 2017/12/11 07:50:46 ralph Exp $
+# ------------------------------------------------------------------------
+# atom:set fenc=utf8 ff=unix ft=shell ts=2 et:
