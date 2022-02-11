b97e19e68434cf628ff8ac5aa3333156dd0da142
roseswe <3810660+roseswe@users.noreply.github.com>
2022-02-11 16:15:40 +0100

    chg: Removed f_mirc etc. added RMS enh: Better error handling

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # rupdater
 
-There is no integrated update mechanism for the programs from rose_swe.
+As there is no integrated update mechanism for the programs from rose_swe we provide the Shell script `rupdater` for the following sites:
 
--> http://rose.rult.at/
+* <http://rose.rult.at/>
 
--> http://www.cfg2html.com
+* <http://www.cfg2html.com>
 
-You can use the shell script rupdater.sh (for http://rose.rult.at) if you have a *NIX environment to download updates of the programs.
+You can use the shell script `rupdater.sh` (for <http://rose.rult.at>) if you have a *NIX environment to download updates of the programs. The scripts relies on standard POSIX commands like wget, awk etc.
 
-$Id: README.md,v 1.3 2020/05/29 13:48:17 ralph Exp $
+$Id: README.md,v 1.4 2022/02/11 15:13:40 ralph Exp $
--- rupdater.sh
+++ rupdater.sh
@@ -1,6 +1,6 @@
-# @(#) $Id: rupdater.sh,v 1.13 2020/05/29 13:48:17 ralph Exp $
+# @(#) $Id: rupdater.sh,v 1.17 2022/02/11 12:49:57 ralph Exp $
 # --=-----------------------------------------------------------------------=---
-# (c) 1997 - 2020 by Ralph Roth  -*- http://rose.rult.at -*-
+# (c) 1997-2022 by Ralph Roth  -*- http://rose.rult.at -*-
 # This script is maintained at: https://github.com/roseswe/rupdater
 
 # There is no integrated update mechanism for our programs. You can use the
@@ -11,28 +11,32 @@ echo "ROSE SWE automatic updater for Linux or Win32/cygwin using wget/awk"
 
 URL=http://rose-swe.bplaced.net/dl
 #   -N,  --timestamping            don't re-retrieve files unless newer than
-wget -qN $URL/md5sums.md5 2>&1 > /dev/null || exit 1
+wget -qN ${URL}/md5sums.md5 2>&1 > /dev/null || exit 1
 
-dos2unix md5sums.md5   > /dev/null    ## in the case we had used the win32 version of dir2html?
+dos2unix md5sums.md5   2> /dev/null    ## in the case we had used the win32 version of dir2html?
 
-for i in rhbvs F_Mirc favboot AntiLink rfwt_ VirScanPlus MemScan CrCheck HackStop hashall
+## 2022: removed: f_mirc, CrCheck, HackStop
+
+## Main/interesting programs:
+for i in rhbvs rms favboot AntiLink rfwt_ VirScanPlus MemScan  hashall MPScan dir2html rADFAQ rPCP rUPP rupdater.sh
 do
-     FILE=$(gawk ' $2 ~ /'${i}'/  { print $2; }; ' md5sums.md5)
+     FILE=$(gawk ' $2 ~ /'${i}'/  { print $2; }; ' md5sums.md5|tail -1)
      # echo "$i=$FILE" # debug
-     if [ ! -r "$FILE" ]
+     if [ ! -r "${FILE}" ]
      then
-        echo "Trying to download new/updated file $FILE (from the $i package)"
-        wget -qN -c $URL/$FILE
+        echo "[!!] ${FILE}: Trying to download new/updated file"
+        timeout 15m  wget -qN -c ${URL}/${FILE}
      else
-        echo "SORRY: $FILE: No newer version available!"
+        echo "[OK] ${FILE}: No newer version available!"
      fi
 done
-
+echo ""
+echo "Downloading finished, checking the downloaded files with MD5SUM..."
 md5sum --quiet -c md5sums.md5  # prints only corrupted files
 rc=$?
 
 exit $rc
 
-# $Header: /home/cvs/src/prj/web_page/rupdater.sh,v 1.13 2020/05/29 13:48:17 ralph Exp $
+# $Header: /home/cvs/src/prj/web_page/rupdater.sh,v 1.17 2022/02/11 12:49:57 ralph Exp $
 # ------------------------------------------------------------------------
-# atom:set fenc=utf8 ff=unix ft=shell ts=2 et:
+# vim:set fenc=utf8 ff=unix ft=shell ts=2 et:
