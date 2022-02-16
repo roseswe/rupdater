11e614100239769661fc59e7f92ba3eb0ce6f157
roseswe <3810660+roseswe@users.noreply.github.com>
2022-02-16 23:05:28 +0100

    chg: fixes for MS-Code/ShellCheck

--- rupdater.sh
+++ rupdater.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 # @(#) $Id: rupdater.sh,v 1.17 2022/02/11 12:49:57 ralph Exp $
 # --=-----------------------------------------------------------------------=---
 # (c) 1997-2022 by Ralph Roth  -*- http://rose.rult.at -*-
 # This script is maintained at: https://github.com/roseswe/rupdater
 
 # There is no integrated update mechanism for our programs. You can use the
-# shell script rupdater.sh if you have a *NIX environment to download updates
+# shell script "rupdater.sh" if you have a *NIX environment to download updates
 # of the program.
 
 echo "ROSE SWE automatic updater for Linux or Win32/cygwin using wget/awk"
 
 URL=http://rose-swe.bplaced.net/dl
 #   -N,  --timestamping            don't re-retrieve files unless newer than
-wget -qN ${URL}/md5sums.md5 2>&1 > /dev/null || exit 1
+wget -qN ${URL}/md5sums.md5 > /dev/null 2>&1 || exit 1
 
 dos2unix md5sums.md5   2> /dev/null    ## in the case we had used the win32 version of dir2html?
 
@@ -25,7 +26,7 @@ do
      if [ ! -r "${FILE}" ]
      then
         echo "[!!] ${FILE}: Trying to download new/updated file"
-        timeout 15m  wget -qN -c ${URL}/${FILE}
+        timeout 15m  wget -qN -c "${URL}/${FILE}"
      else
         echo "[OK] ${FILE}: No newer version available!"
      fi
